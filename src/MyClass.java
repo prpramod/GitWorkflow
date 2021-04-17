690d1ea8184345344154d6d28c64c76cd7799398
prpramod <pramod.luffy@gmail.com>
2021-04-17 14:48:31 +0530

    Adding a new line

--- src/MyClass.java
+++ src/MyClass.java
@@ -2,5 +2,6 @@ public class MyClass {
 
     public static void main(String[] args) {
         System.out.println("Pramod welcomes you to git");
+        System.out.println("second code change");
     }
 }
